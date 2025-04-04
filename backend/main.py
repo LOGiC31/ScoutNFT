93b418698b7ff0406184b65fbd205f1939ad5dc8
Simran Jot Kaur <simranjotkaur@Simrans-MacBook-Pro-2.local>
2025-04-04 18:01:44 -0500

    change main.py

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-from supabase.client import supabase
+# from supabase.client import supabase
+from supabase import create_client, Client
+import os
+
+
 app = FastAPI()
+
+
+SUPABASE_URL = "https://eyojtzfkrhlqhnzluivm.supabase.co"
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV5b2p0emZrcmhscWhuemx1aXZtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM3OTc4OTgsImV4cCI6MjA1OTM3Mzg5OH0.rXxoObcjK3wCyYuyhTn6IgOSgWjvF8aAfWJPr-jrG6Y"
+
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 # Enable CORS to allow frontend to communicate with backend
 app.add_middleware(
@@ -20,8 +30,9 @@ class NFTRequest(BaseModel):
 def recommend(req: NFTRequest):
     return {"recommendations": [1, 2, 3]}
 
-
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 @app.get("/nfts")
 async def get_nfts():
     res = supabase.table("nfts").select("*").execute()
+    print(res)
     return res.data
--- backend/supabase/client.py
+++ backend/supabase/client.py
@@ -1,11 +1,8 @@
 # backend/supabase/client.py
 import os
 from supabase import create_client, Client
-from dotenv import load_dotenv
 
-load_dotenv()  # Load from .env file
-
-SUPABASE_URL: str = os.getenv("SUPABASE_URL")
-SUPABASE_KEY: str = os.getenv("SUPABASE_SERVICE_ROLE_KEY")
+SUPABASE_URL: str = os.getenv("https://eyojtzfkrhlqhnzluivm.supabase.co")
+SUPABASE_KEY: str = os.getenv("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV5b2p0emZrcmhscWhuemx1aXZtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM3OTc4OTgsImV4cCI6MjA1OTM3Mzg5OH0.rXxoObcjK3wCyYuyhTn6IgOSgWjvF8aAfWJPr-jrG6Y")
 
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
